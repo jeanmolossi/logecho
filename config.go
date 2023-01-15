f947bee76a35525889e336deb67073af0bda79e6
Jean <jean.carlosmolossi@gmail.com>
2023-01-15 20:02:45 -0300

    docs(config): improve docs

--- config.go
+++ config.go
@@ -5,28 +5,79 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+// defaultConfig set up the production default config
 var defaultConfig ZapConfig = ZapConfig{
 	IsDevelopment: false,
 	Encoding:      JSON,
 }
 
+// DevConfig set up a development builtin config
 var DevConfig ZapConfig = ZapConfig{
 	IsDevelopment: true,
 	Level:         zap.DebugLevel,
 	Encoding:      Text,
 }
 
+// ZapConfig defines the base config to our Logger
+//
+// You can set Keys to preset
+//
+//	Message
+//	Caller
+//	Time
+//
+// Key overrides only apply to key value log formats
 type ZapConfig struct {
+	// IsDevelopment as the property describes, is to
+	// set development configs
 	IsDevelopment bool
 
+	// MessageKey will change the prefix "message"
+	// on the log.
+	//
+	// Only applied to key value log formats
 	MessageKey string
-	CallerKey  string
-	TimeKey    string
+	// CallerKey will change the prefix "caller"
+	// on the log. It can be empty to remove caller
+	// from log message
+	//
+	// Only applied to key value log formats
+	CallerKey string
+	// TimeKey will change the prefix "timestamp"
+	// on the log.
+	//
+	// Only applied to key value log formats
+	TimeKey string
 
-	Level       zapcore.Level
-	EncodeTime  zapcore.TimeEncoder
+	// Level sets the level of logs who should appear
+	//
+	// Default is `0` who is the same as INFO
+	Level zapcore.Level
+
+	// EncodeTime is a option to change how the timestamp
+	// will be formatted.
+	//
+	// Default is RFC3339-formatted string
+	EncodeTime zapcore.TimeEncoder
+
+	// EncodeLevel defines how de format will be printed
+	//
+	// Default is Level serializer to a lowercase string
 	EncodeLevel zapcore.LevelEncoder
-	Encoding    Encoding
+
+	// Encoding changes the log format.
+	//
+	// Can be any of:
+	//
+	// 	JSON
+	// 	Text
+	//
+	// Defaults
+	//
+	// Development environments Text is default
+	//
+	// Production (same as IsDevelopment = false) JSON is default
+	Encoding Encoding
 }
 
 func (z ZapConfig) msgKey() string {

9e56031f054728a81d7b4af02aa879b8f4295a53
Jean <jean.carlosmolossi@gmail.com>
2023-01-16 00:33:51 -0300

    docs(middleware): added middleware config

--- middleware.go
+++ middleware.go
@@ -2,11 +2,47 @@ package logecho
 
 import "github.com/labstack/echo/v4"
 
+// MiddlewareConfig is a struct to set a custom
+// config to echo.Middleware if you want to remove or add a config.
 type MiddlewareConfig struct {
-	EnableLatency      bool
-	EnableRequestCount bool
-	EnableRequestID    bool
+	// EnableLatency will enable latency calc.
+	//
+	// IMPORTANT: To see latency at log point you should
+	// add the following to your Fields template:
+	//
+	// 	logecho.Fields{
+	// 		"latency": logecho.LatencyString, // or any latency field
+	// 	}
+	EnableLatency bool
 
+	// EnableRequestCount will enable request counter. It counts
+	// how many request are running and was run in current transcation.
+	//
+	// Look to the examples:
+	//
+	// E.g.1.: Your app receive a single request, then you have the
+	// following metrics:
+	//
+	//	- running requests	-> 1
+	//	- transaction requests -> 1
+	//
+	// E.g.2.: Your app receive 100 requests, but 20 was finished. Then you
+	// have the following metrics
+	//
+	//	- running requests	-> 80
+	//	- transaction requests -> 100
+	EnableRequestCount bool
+
+	// EnableRequestID will enable transaction request ID. It will
+	// set request ID on the context and the response header X-Request-ID.
+	//
+	// Useful to tracing all logs from a single request.
+	EnableRequestID bool
+
+	// Fields will set how aditional fields will be printed on log
+	// messages.
+	//
+	// It set's the key and what will be logged
 	Fields Fields
 }
 
@@ -28,10 +64,15 @@ var (
 	}
 )
 
+// Middleware is a default config middleware. Look the default configs:
+//
+// RequestID, Latency and RequestCount are enabled by default
 func Middleware() echo.MiddlewareFunc {
 	return MiddlewareWithConfig(defaultMiddlewareConfig)
 }
 
+// MiddlewareWithTemplate is the middleware default config, but in this
+// you can set up the template fields who you want in log
 func MiddlewareWithTemplate(tpl Fields) echo.MiddlewareFunc {
 	cfg := defaultMiddlewareConfig
 	cfg.Fields = tpl
@@ -39,6 +80,11 @@ func MiddlewareWithTemplate(tpl Fields) echo.MiddlewareFunc {
 	return MiddlewareWithConfig(cfg)
 }
 
+// MiddlewareWithConfig is the middleware with a custom config.
+//
+// It will set default template if has no Fields in the config.
+//
+// Additional that it will log message "request done" on end call
 func MiddlewareWithConfig(cfg MiddlewareConfig) echo.MiddlewareFunc {
 	if len(cfg.Fields) == 0 {
 		cfg.Fields = defaultTpl
